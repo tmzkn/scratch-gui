bcc80c2fc1cf28308f08a768374dd7ea352fcab8
Burak Yildiz <0byldz@gmail.com>
2020-07-26 12:42:25 +0200

    set default lang to tr

--- src/lib/detect-locale.js
+++ src/lib/detect-locale.js
@@ -12,7 +12,7 @@ import queryString from 'query-string';
  * @return {string} the preferred locale
  */
 const detectLocale = supportedLocales => {
-    let locale = 'en'; // default
+    let locale = 'tr'; // default
     let browserLocale = window.navigator.userLanguage || window.navigator.language;
     browserLocale = browserLocale.toLowerCase();
     // try to set locale from browserLocale
